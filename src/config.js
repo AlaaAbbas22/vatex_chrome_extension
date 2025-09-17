5d991c540980377168ba425015351cbb27fca69a
AlaaAbbas22 <alaa@uni.minerva.edu>
2025-09-16 21:36:15 -0700

    added some comments

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// Configuration settings for the extension
-const config = {
-  // API endpoints
-  apiUrl: 'http://localhost:5000',
-  clientUrl: 'http://localhost:3000',
-};
-
-export default config;
